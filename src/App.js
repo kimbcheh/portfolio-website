c13ad9e500814cd31e7d479e1aefdd82b1d2ec39
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-07-02 15:00:36 +1000

    feat: create header component (#2)
    * create header component with link data
    
    * import header component into app
    
    * remove console.log for link data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss'
+import Header from './components/Header'
 
 function App() {
  return (
   <div>
-   <header>placeholder header</header>
+   <Header />
    <main>
     <section>placeholder projects section</section>
     <section>placeholder about section</section>
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import { linkData } from '../data'
+
+function Header() {
+ return (
+  <header>
+   <nav>
+    <h1>KIMBERLY CHEH</h1>
+    <h1>KIMB.</h1>
+    <ul>
+     {linkData.map((link) => {
+      return (
+       <li key={link.title}>
+        <a href={link.url}>{link.title}</a>
+       </li>
+      )
+     })}
+    </ul>
+   </nav>
+   <p>
+    Kimberly is a front-end developer based in Melbourne, Australia &#128171;
+   </p>
+  </header>
+ )
+}
+
+export default Header
