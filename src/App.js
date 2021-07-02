a23bdff5cf0718248de240f77d82aa8242f7fb72
kimbcheh <kimbcheh@gmail.com>
2021-07-02 15:03:00 +1000

    create projects component and import into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.scss'
 import Header from './components/Header'
+import Projects from './components/Projects'
 
 function App() {
  return (
   <div>
    <Header />
    <main>
-    <section>placeholder projects section</section>
+    <Projects />
     <section>placeholder about section</section>
    </main>
    <footer>placeholder footer</footer>
---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import { projectData } from '../data'
+
+function Projects() {
+ return (
+  <section>
+   <p>Projects</p>
+  </section>
+ )
+}
+
+export default Projects
