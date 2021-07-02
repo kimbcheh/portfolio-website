4b1e23b27e6b617b621bde3dd93363344b02c44f
kimbcheh <kimbcheh@gmail.com>
2021-07-02 16:19:01 +1000

    add placeholder text in about component

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,13 @@ function About() {
  return (
   <section>
    <h2>ABOUT</h2>
+   <p>
+    Avocado basil pesto winter apples lime seitan hearts of palm one bowl cumin
+    arugula salad sweet potato avocado pesto peaches sesame soba noodles red
+    amazon pepper miso turmeric glazed aubergine entree avocado dressing drizzle
+    pinch of yum cozy cinnamon oatmeal crispy iceberg lettuce lentils chili
+    crispy.
+   </p>
   </section>
  )
 }
