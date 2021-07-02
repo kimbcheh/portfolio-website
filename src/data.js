405028d9cc2489194c667951b9ac84fa545c0a86
kimbcheh <kimbcheh@gmail.com>
2021-07-02 12:42:01 +1000

    add placeholder project and link data

---
+++ src/data.js
@@ -0,0 +1,53 @@
+export const projectData = [
+ {
+  title: 'Lucid Weather',
+  tech: 'REACT / CSS / AXIOS',
+  description:
+   "A simple web app for displaying current and future weather data for any Australian city. The app's background will change according to the current weather conditions. Built using the OpenWeather API.",
+  demo: 'https://lucid-weather.netlify.app/',
+  github: 'https://github.com/kimbcheh/lucid-weather',
+  image: 'placeholder',
+ },
+ {
+  title: 'Project 2',
+  tech: 'HTML / CSS / JAVASCRIPT',
+  description:
+   'Roasted peanuts red pepper cocoa Southern Italian Mexican fiesta chai tea walnut pesto tart coconut refreshing cucumber splash lemon red lentil soup paprika spicy red amazon pepper salad lemon lime minty blueberry chia seed jam coriander tahini drizzle cinnamon cumin dill.',
+  demo: '/',
+  github: '/',
+  image: 'placeholder image',
+ },
+ {
+  title: 'Project 3',
+  tech: 'HTML / CSS / JAVASCRIPT',
+  description:
+   'Peaches balsamic vinaigrette instant pot grapefruit morning smoothie bowl basmati mint lime taco salsa miso turmeric glazed aubergine spring naga viper macadamia nut cookies second course cashew strawberry mango smoothie',
+  demo: '/',
+  github: '/',
+  image: 'placeholder',
+ },
+ {
+  title: 'Project 4',
+  tech: 'HTML / CSS / JAVASCRIPT',
+  description:
+   'Fiery fruit strawberry spinach salad casserole apple vinaigrette miso dressing hot red curry tofu noodles mocha chocolate strawberry mango smoothie portobello mushrooms orange asian pear chilies tomato and basil chai tea spicy candy cane winter burritos coconut milk.',
+  demo: '/',
+  github: '/',
+  image: 'placeholder',
+ },
+]
+
+export const linkData = [
+ {
+  title: 'LINKEDIN',
+  url: '/',
+ },
+ {
+  title: 'GITHUB',
+  url: 'https://github.com/kimbcheh',
+ },
+ {
+  title: 'RESUME',
+  url: '/',
+ },
+]
