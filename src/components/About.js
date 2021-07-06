7e099c960926909489ef954b4a6394c0ed2b067d
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-07-06 19:20:33 +1000

    feat: add about description and skills list (#9)
    * add image for lucid weather project
    
    * remove placeholder projects
    
    * remove linkedin link
    
    * add black border to project images
    
    * remove black border from project images
    
    * add styling for about component
    
    * add about me description and list of skills
    
    * adjust padding for skills section
    
    * Add dash symbol to skills section

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,31 @@ function About() {
    <h2>
     ABOUT ME <span className='symbol'>&#x2193;</span>
    </h2>
-   <p className='about-text'>
-    Avocado basil pesto winter apples lime seitan hearts of palm one bowl cumin
-    arugula salad sweet potato avocado pesto peaches sesame soba noodles red
-    amazon pepper miso turmeric glazed aubergine entree avocado dressing drizzle
-    pinch of yum cozy cinnamon oatmeal crispy iceberg lettuce lentils chili
-    crispy.
-   </p>
+   <div className='about-text'>
+    <p>
+     Hi I'm Kimberly! I'm a detail-oriented professional looking to pivot into
+     software development. I have undertaken self-directed learning in various
+     front-end languages and tools and am focused on continually improving my
+     skills.
+    </p>
+    <p>
+     From my previous work experience, I bring analytical thinking,
+     problem-solving skills and clear communication to the table.
+    </p>
+    <p>
+     On a more personal note, I'm a huge fan of good design and coffee! &#9749;
+    </p>
+    <p>
+     Some technology I've worked with...
+     <ul>
+      <li>— LANGUAGES: HTML5, CSS3 AND JAVASCRIPT ES6</li>
+      <li>— FRAMEWORKS: REACT</li>
+      <li>
+       — OTHER: VERSION CONTROL, RESTFUL API, CSS FRAMEWORKS, SASS AND FIGMA
+      </li>
+     </ul>
+    </p>
+   </div>
   </section>
  )
 }
