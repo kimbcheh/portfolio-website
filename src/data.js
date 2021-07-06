74384c3d13cc62346dfb9bddb29609786db9011f
kimbcheh <kimbcheh@gmail.com>
2021-07-06 14:52:27 +1000

    remove placeholder projects

--- src/data.js
+++ src/data.js
@@ -8,33 +8,6 @@ export const projectData = [
   github: 'https://github.com/kimbcheh/lucid-weather',
   image: 'lucid-weather.jpg',
  },
- {
-  title: 'Project 2',
-  tech: 'HTML / CSS / JAVASCRIPT',
-  description:
-   'Roasted peanuts red pepper cocoa Southern Italian Mexican fiesta chai tea walnut pesto tart coconut refreshing cucumber splash lemon red lentil soup paprika spicy red amazon pepper salad lemon lime minty blueberry chia seed jam coriander tahini drizzle cinnamon cumin dill.',
-  demo: '/',
-  github: '/',
-  image: 'placeholder.jpeg',
- },
- {
-  title: 'Project 3',
-  tech: 'HTML / CSS / JAVASCRIPT',
-  description:
-   'Peaches balsamic vinaigrette instant pot grapefruit morning smoothie bowl basmati mint lime taco salsa miso turmeric glazed aubergine spring naga viper macadamia nut cookies second course cashew strawberry mango smoothie',
-  demo: '/',
-  github: '/',
-  image: 'placeholder.jpeg',
- },
- {
-  title: 'Project 4',
-  tech: 'HTML / CSS / JAVASCRIPT',
-  description:
-   'Fiery fruit strawberry spinach salad casserole apple vinaigrette miso dressing hot red curry tofu noodles mocha chocolate strawberry mango smoothie portobello mushrooms orange asian pear chilies tomato and basil chai tea spicy candy cane winter burritos coconut milk.',
-  demo: '/',
-  github: '/',
-  image: 'placeholder.jpeg',
- },
 ]
 
 export const linkData = [
