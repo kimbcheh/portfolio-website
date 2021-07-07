84c7f298cdf1794dcc4aa5453bce7f935da19770
kimbcheh <kimbcheh@gmail.com>
2021-07-07 22:09:16 +1000

    remove links section in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,6 @@ function Header({ linkData }) {
     <span className='star'>&#x2736;</span>
     <h1 className='desktop-heading'> KIMBERLY CHEH</h1>
     <h1 className='mobile-heading'> KIMB.</h1>
-    <ul>
-     {linkData.map((link) => {
-      return (
-       <li key={link.title}>
-        <a href={link.url}>{link.title}</a>{' '}
-        <span className='symbol'>&#x2197;</span>
-       </li>
-      )
-     })}
-    </ul>
    </nav>
   </header>
  )
