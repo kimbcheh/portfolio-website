0a4961a03c8a2b4151233b215eb8e8dbf0ea3045
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-07-08 19:29:20 +1000

    feat: move about component and change styling (#13)
    * move about component above project component
    
    * move hero container and text into about component
    
    * remove links section in nav
    
    * delete linKData array
    
    * move links into about component
    
    * change max-width of body
    
    * change background color
    
    * change font to work sans and ibm plex mono
    
    * add accent color
    
    * remove nav element
    
    * change project links format
    
    * change test to reflect change in links
    
    * change wording of about links
    
    * refactor css according to changes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import Header from './components/Header'
 import Projects from './components/Projects'
 import About from './components/About'
 import Footer from './components/Footer'
-import { linkData, projectData } from './data'
+import { projectData } from './data'
 
 function App() {
  return (
   <div>
-   <Header linkData={linkData} />
+   <Header />
    <main>
-    <Projects projectData={projectData} />
     <About />
+    <Projects projectData={projectData} />
    </main>
    <Footer />
   </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,9 @@
 function Header({ linkData }) {
  return (
-  <header>
-   <nav className='bottom-border'>
-    <span className='star'>&#x2736;</span>
-    <h1 className='desktop-heading'> KIMBERLY CHEH</h1>
-    <h1 className='mobile-heading'> KIMB.</h1>
-    <ul>
-     {linkData.map((link) => {
-      return (
-       <li key={link.title}>
-        <a href={link.url}>{link.title}</a>{' '}
-        <span className='symbol'>&#x2197;</span>
-       </li>
-      )
-     })}
-    </ul>
-   </nav>
-   <div className='hero-container bottom-border'>
-    <p className='hero-text'>
-     Kimberly is a front-end developer based in Melbourne, Australia &#128171;
-    </p>
-   </div>
+  <header className='bottom-border'>
+   <span className='star'>&#x2736;</span>
+   <h1 className='desktop-heading'> KIMBERLY CHEH</h1>
+   <h1 className='mobile-heading'> KIMB.</h1>
   </header>
  )
 }
