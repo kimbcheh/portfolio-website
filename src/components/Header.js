232382e91685bc993ae5415ca9115e5d5efea770
kimbcheh <kimbcheh@gmail.com>
2021-07-08 19:21:28 +1000

    remove nav element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 function Header({ linkData }) {
  return (
-  <header>
-   <nav className='bottom-border'>
-    <span className='star'>&#x2736;</span>
-    <h1 className='desktop-heading'> KIMBERLY CHEH</h1>
-    <h1 className='mobile-heading'> KIMB.</h1>
-   </nav>
+  <header className='bottom-border'>
+   <span className='star'>&#x2736;</span>
+   <h1 className='desktop-heading'> KIMBERLY CHEH</h1>
+   <h1 className='mobile-heading'> KIMB.</h1>
   </header>
  )
 }
